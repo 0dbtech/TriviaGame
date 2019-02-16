ccf0c74be2408a6bdcc54781ae8ec21295758b12
admin <admin@admins-MacBook-Pro.local>
2019-02-16 00:21:16 -0500

    getting there

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,10 +13,14 @@ var Q1 = {
       answer2:"Q1 Answer 2",
       answer3:"Q1 Answer 3",
       answer4:"Q1 Answer 4",
-      correctAnswer:"Q1 correct answer is",
+      correctAnswer: function(){
+            return this.answer1;
+      },
 
 };
-console.log("testing Q object " + Q1.question);
+
+//access correct answer method
+console.log("testing Q object " + Q1.correctAnswer());
 
 var Q2 = {
       question:"This is Q2",
@@ -24,10 +28,12 @@ var Q2 = {
       answer2:"Q2 Answer 2",
       answer3:"Q2 Answer 3",
       answer4:"Q2 Answer 4",
-      correctAnswer:"Q2 correct answer is",
+      correctAnswer: function(){
+            return this.answer2;
+      },
 
 };
-console.log("testing Q object " + Q2.question);
+console.log("testing Q object " + Q2.correctAnswer());
 
 var Q3 = {
       question:"This is Q3",
@@ -35,10 +41,12 @@ var Q3 = {
       answer2:"Q3 Answer 2",
       answer3:"Q3 Answer 3",
       answer4:"Q3 Answer 4",
-      correctAnswer:"Q3 correct answer is",
+      correctAnswer: function(){
+            return this.answer3;
+      },
 
 };
-console.log("testing Q object " + Q3.question);
+console.log("testing Q object " + Q3.correctAnswer());
 
 var Q4 = {
       question:"This is Q4",
@@ -46,10 +54,11 @@ var Q4 = {
       answer2:"Q4 Answer 2",
       answer3:"Q4 Answer 3",
       answer4:"Q4 Answer 4",
-      correctAnswer:"Q4 correct answer is",
-
+      correctAnswer: function(){
+            return this.answer4;
+      },
 };
-console.log("testing Q object " + Q4.question);
+console.log("testing Q object " + Q4.correctAnswer());
 
 //array for question objects
 var questionsArray = [Q1,Q2,Q3,Q4];
@@ -58,12 +67,16 @@ console.log("testing questions array " + questionsArray[0].question);
 //Question Objects END
 
 //variables
-var unanswered = 0;
-var correct = 0;
-var incorrect = 0;
+var unAnsweredCount = 0;
+var correctCount = 0;
+var wrongCount = 0;
 var isGameStarted = false;
-var correctAnswerTest = "Answer 1 is correct"
-var currentQuestion = questionsArray[0];
+
+//index of current question
+var iQuestionsArray = 0
+
+
+var currentQuestion = questionsArray[iQuestionsArray];
 
 console.log("current question is " + currentQuestion.question);
 
@@ -84,8 +97,8 @@ function startGame(){
       console.log("starting game");
       isGameStarted = true;
       $('#start').hide();
-      $('.trivia-dom').show();
-      $('.timer-dom').show();
+      // $('.trivia-dom').show();
+      $('#timer-dom').show();
       loadQuestion();
 
       return;
@@ -94,24 +107,62 @@ function startGame(){
 
 function wrongAnswer(){
       console.log("playing WRONG answer");
+      $('.trivia-dom').hide();
+      $("#wrong-dom").append("<h2>Wrong Answer!</h2>");
+      $("#wrong-dom").append("<h2>The correct answer is " + currentQuestion.correctAnswer() + "</h2>");
+      $('#wrong-dom').show();
+      runTimer2();
+      wrongCount++;
 return;
 }      
 
 function correctAnswer(){
       console.log("playing CORRECT answer");
+      $('.trivia-dom').hide();
+      $('#correct-dom').append("<h2>CORRECT!</h2>");
+      $("#correct-dom").append("<h2>" + currentQuestion.correctAnswer() + " is Correct!</h2>");
+      $('#correct-dom').show();
+      runTimer2();
+      correctCount++;
 return;
 }  
 
-function selectQuestion(){
-      console.log("selecting Question");
-
+function unAnswered(){
+      console.log("unanswered time up");
+      $('.trivia-dom').hide();
+      $("#timeup-dom").append("<h2>TIME IS UP!</h2>");
+      $("#timeup-dom").append("<h2>Appending</h2>");
+      $('#timeup-dom').show();
+      runTimer2();
+      unAnsweredCount++;
 return;
-} 
+}      
 
+
+
+function nextQuestion(){
+      
+            iQuestionsArray++;
+            console.log("iQuestionsArray value is " + iQuestionsArray);
+            currentQuestion = questionsArray[iQuestionsArray];
+            console.log("Next Question is " + currentQuestion.question);
+            loadQuestion();
+      return;
+      }
+
+//Under construction
 function checkAnswer(){
       console.log("checking Answer");
+      //PAUSE TIMER 1
+      runTimer2();
+      wrongAnswer();
 return;
 } 
+
+function endGame(){
+      console.log("Game Over");
+      return;   
+}
 
 // function updateDOM(){
 //       $("#start").hide();
@@ -141,23 +192,171 @@ return;
 //       }
 
 function loadQuestion (){
-      $('#question-dom').html("<h2>" + Q1.question + "</h2>");
+      $('#correct-dom').hide();
+      $("#correct-dom").empty();
+      $('#wrong-dom').hide();
+      $("#wrong-dom").empty();
+      $('#timeup-dom').hide();
+      $("#timeup-dom").empty();
 
-      $('#answer1-dom').attr('data-answer', Q1.answer1);
-      $('#answer1-dom').html("<h2>" + Q1.answer1 + "</h2>");
+      $('.trivia-dom').show();
+      $('#question-dom').html("<h2>" + currentQuestion.question + "</h2>");
 
-      $('#answer2-dom').attr('data-answer', Q1.answer2);
-      $('#answer2-dom').html("<h2>" + Q1.answer2 + "</h2>");
+      $('#answer1-dom').attr('data-answer', currentQuestion.answer1);
+      $('#answer1-dom').html("<h2>" + currentQuestion.answer1 + "</h2>");
 
-      $('#answer3-dom').attr('data-answer', Q1.answer3);
-      $('#answer3-dom').html("<h2>" + Q1.answer3 + "</h2>");
+      $('#answer2-dom').attr('data-answer', currentQuestion.answer2);
+      $('#answer2-dom').html("<h2>" + currentQuestion.answer2 + "</h2>");
 
-      $('#answer4-dom').attr('data-answer', Q1.answer4);
-      $('#answer4-dom').html("<h2>" + Q1.answer4 + "</h2>");
+      $('#answer3-dom').attr('data-answer', currentQuestion.answer3);
+      $('#answer3-dom').html("<h2>" + currentQuestion.answer3 + "</h2>");
 
-    return;     
+      $('#answer4-dom').attr('data-answer', currentQuestion.answer4);
+      $('#answer4-dom').html("<h2>" + currentQuestion.answer4 + "</h2>");
+
+      //start clock
+      runTimer();
+
+//test scores
+
+    console.log("correctCount: " + correctCount);
+    console.log("wrongCount: " + wrongCount);
+    console.log("unAnsweredCount: " + unAnsweredCount);
+    console.log("current question is " + currentQuestion.question);
+    
+    return; 
+    //END loadQuestion    
       }
 
+
+//TIMER START////////////
+
+    //  Interval Demonstration
+    //  Set our number counter to 100.
+//enter time     
+    var timerNumber = 11;
+
+
+    //  Variable that will hold our interval ID when we execute
+    //  the "run" function
+    var intervalId;
+
+    //  When the stop button gets clicked, run the stop function.
+            //     $("#stop").on("click", stop);
+
+    //  When the resume button gets clicked, execute the run function.
+            //     $("#resume").on("click", run);
+
+    //  The run function sets an interval
+    //  that runs the decrement function once a second.
+    //  *****BUG FIX******** 
+    //  Clearing the intervalId prior to setting our new intervalId will not allow multiple instances.
+    function runTimer() {
+      clearInterval(intervalId);
+      intervalId = setInterval(decrement, 1000);
+//prevent negative counter
+//enter time again     
+     timerNumber = 11;
+
+
+    }
+
+    //Between questions timer
+    function runTimer2() {
+      timerNumber = 3;
+      clearInterval(intervalId);
+      intervalId = setInterval(decrement2, 1000);
+//prevent negative counter
+//enter time again     
+    
+
+
+    }    
+
+//DECREMENT Question
+    //  The decrement function.
+    function decrement() {
+
+      //  Decrease number by one.
+      timerNumber--;
+
+      //  Show the number in the #timer-dom tag.
+      $("#timer-dom").html("<h2>Time remaining:  " + timerNumber + "</h2>");
+
+
+      //  Once number hits zero...
+      if (timerNumber === 0) {
+
+        //  ...run the stop function.
+        stop();
+
+        //  Alert the user that time is up.
+
+////ADD answer select functions here        
+      //   alert("Time Up!");
+
+//test score updates
+      //   correctAnswer();
+      //   wrongAnswer();
+      //   unAnswered();
+        unAnswered();
+      //   loadQuestion();
+
+        //Next question
+      //   loadQuestion();
+      }
+    }
+
+//DECREMENT 2 Between Questions
+    //  The decrement function.
+    function decrement2() {
+
+      //  Decrease number by one.
+      timerNumber--;
+
+      // //  Show the number in the #timer-dom tag.
+      // $("#timer-dom").html("<h2>Time remaining:  " + timerNumber + "</h2>");
+
+
+      //  Once number hits zero...
+      if (timerNumber === 0) {
+
+        //  ...run the stop function.
+        stop();
+
+        //  Alert the user that time is up.
+
+////ADD answer select functions here        
+      //   alert("Time Up!");
+
+//test score updates
+      //   correctAnswer();
+      //   wrongAnswer();
+      //   unAnswered();
+        nextQuestion();
+      //   loadQuestion();
+
+        //Next question
+      //   loadQuestion();
+      }
+    }
+
+    //  The stop function
+    function stop() {
+
+      //  Clears our intervalId
+      //  We just pass the name of the interval
+      //  to the clearInterval function.
+      clearInterval(intervalId);
+//unanswered?      
+    }
+
+  
+
+//TIMER END////////
+
+
+/////////////////////////////////////////////////////      
 //PAGE LOAD
 pageLoad();
 
@@ -176,6 +375,8 @@ $(".answers-dom").on("click", function() {
       // console.log("coin value is " + coinValue );
       // scoreCheck();  
       console.log("You selected " + selectedAnswer);
+      checkAnswer();
+      return;
       });
 
 
