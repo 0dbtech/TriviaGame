61b5fb58f1660e0d53e80ec6c87c0ef1a7ec04e4
admin <admin@admins-MacBook-Pro.local>
2019-02-16 20:01:47 -0500

    template working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,7 +16,9 @@ var Q1 = {
       correctAnswer: function(){
             return this.answer1;
       },
-
+      //responsive off
+      gif:'<iframe src="https://giphy.com/embed/l46C5G0CQoniAisP6" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/studiosoriginals-subway-queens-action-bronson-l46C5G0CQoniAisP6">via GIPHY</a></p>',
+      
 };
 
 //access correct answer method
@@ -31,6 +33,8 @@ var Q2 = {
       correctAnswer: function(){
             return this.answer2;
       },
+      //responsive off
+      gif:'',
 
 };
 console.log("testing Q object " + Q2.correctAnswer());
@@ -44,6 +48,8 @@ var Q3 = {
       correctAnswer: function(){
             return this.answer3;
       },
+       //responsive off
+       gif:'',
 
 };
 console.log("testing Q object " + Q3.correctAnswer());
@@ -57,7 +63,10 @@ var Q4 = {
       correctAnswer: function(){
             return this.answer4;
       },
+       //responsive off
+       gif:'',
 };
+
 console.log("testing Q object " + Q4.correctAnswer());
 
 //array for question objects
@@ -66,12 +75,11 @@ console.log("testing questions array " + questionsArray[0].question);
 
 //Question Objects END
 
-//variables
+//GLOBAL variables
 var unAnsweredCount = 0;
 var correctCount = 0;
 var wrongCount = 0;
-var isGameStarted = false;
-
+var selectedAnswer = "";
 //index of current question
 var iQuestionsArray = 0
 
@@ -82,37 +90,99 @@ console.log("current question is " + currentQuestion.question);
 
 //functions START
 function pageLoad(){
-      if (isGameStarted === false){
+     
       console.log("page load");
       $("#start").html("<h3>Start Game</h3>");
       $('#timer-dom').hide();
       $('.trivia-dom').hide();
+      // $('.trivia-dom').empty();
       $('.score-dom').hide();
-      }
+      $('.score-dom').empty();
+ 
+
       return;
 
       }
 
 function startGame(){
       console.log("starting game");
-      isGameStarted = true;
+      
+           //reset gloabl vars for new game DO NOT initialize
+           unAnsweredCount = 0;
+           correctCount = 0;
+           wrongCount = 0;
+           selectedAnswer = "";
+           iQuestionsArray = 0;
+            currentQuestion = questionsArray[iQuestionsArray];
+      console.log("iQuestionsArray:" + iQuestionsArray);     
+      //show answer result container class for new games, populated between questions by append elements
+      $('.qresults-dom').show();
       $('#start').hide();
+      $('.score-dom').hide();
+      $('.score-dom').empty();
       // $('.trivia-dom').show();
       $('#timer-dom').show();
+      $('.trivia-dom').show();
       loadQuestion();
 
       return;
       // isGameStarted = true;
       }
 
+function isGameOver(){
+     
+      if (iQuestionsArray !== questionsArray.length-1){
+            console.log("not finished yet, next question " + iQuestionsArray + " " + questionsArray.length);
+            nextQuestion();
+            
+            }
+      else{
+     showResults()
+      }
+      
+      return;
+}
+
+function showResults (){
+      $('.trivia-dom').hide();
+      $('#timer-dom').hide();
+      $('.score-dom').append("<h2>Here's how well you know NYC:</h2>");
+      $('.score-dom').append("<h2>Correct Answers: " + correctCount + "</h2>");
+      $('.score-dom').append("<h2>Wrong Answers: " + wrongCount + "</h2>");
+      $('.score-dom').append("<h2>Unanswered: " + unAnsweredCount + "</h2>");
+      $('.qresults-dom').hide();
+      $('.score-dom').show();
+      $('#start').html("<h3>Play Again</h3>");
+      $('#start').show();
+
+  
+      // var unAnsweredCount = 0;
+      // var correctCount = 0;
+      // var wrongCount = 0;
+}
+
+//media functions
+
+//END media functions
+
 function wrongAnswer(){
       console.log("playing WRONG answer");
       $('.trivia-dom').hide();
       $("#wrong-dom").append("<h2>Wrong Answer!</h2>");
       $("#wrong-dom").append("<h2>The correct answer is " + currentQuestion.correctAnswer() + "</h2>");
+      //note 'single' inside "double"
+      //video source relative path based on html location
+      $("#wrong-dom").append("<video id='vid-dom' width='320' height='176' autoplay><source src='assets/videos/wrong.mp4' type='video/mp4'></video>");
+      var vid = document.getElementById("vid-dom");
+      function playVid() {
+            vid.play();
+            }
       $('#wrong-dom').show();
       runTimer2();
+      playVid();
       wrongCount++;
+      
+      
 return;
 }      
 
@@ -121,6 +191,7 @@ function correctAnswer(){
       $('.trivia-dom').hide();
       $('#correct-dom').append("<h2>CORRECT!</h2>");
       $("#correct-dom").append("<h2>" + currentQuestion.correctAnswer() + " is Correct!</h2>");
+      $("#correct-dom").append(currentQuestion.gif);
       $('#correct-dom').show();
       runTimer2();
       correctCount++;
@@ -131,7 +202,7 @@ function unAnswered(){
       console.log("unanswered time up");
       $('.trivia-dom').hide();
       $("#timeup-dom").append("<h2>TIME IS UP!</h2>");
-      $("#timeup-dom").append("<h2>Appending</h2>");
+      $("#timeup-dom").append("<h2>The correct answer is " + currentQuestion.correctAnswer() + "</h2>");
       $('#timeup-dom').show();
       runTimer2();
       unAnsweredCount++;
@@ -141,64 +212,52 @@ return;
 
 
 function nextQuestion(){
-      
+     
             iQuestionsArray++;
             console.log("iQuestionsArray value is " + iQuestionsArray);
+
+               
+
             currentQuestion = questionsArray[iQuestionsArray];
             console.log("Next Question is " + currentQuestion.question);
+            console.log("checking value at game end " + iQuestionsArray);
+            
             loadQuestion();
+      
+            
       return;
       }
 
 //Under construction
 function checkAnswer(){
       console.log("checking Answer");
+      
       //PAUSE TIMER 1
-      runTimer2();
+      if (selectedAnswer === currentQuestion.correctAnswer()){
+      correctAnswer()
+      
+      }
+      else {
       wrongAnswer();
-return;
+      
+      }
+      runTimer2();
+      return;
 } 
 
-function endGame(){
-      console.log("Game Over");
-      return;   
-}
 
-// function updateDOM(){
-//       $("#start").hide();
-//       $("#target-number").html("<h2>Target Coin: " + targetNum + "</h2>");
-//       $("#wins-dom").html("<h2>Wins: " + wins + "</h2>");
-//       $("#loss-dom").html("<h2>Losses: " + losses + "</h2>");
-//       $("#counter").html("<h2>Crypto Counter: " + counter + "</h2>");
-//       return;
-//       }
 
-// function scoreCheck(){
 
-//       if (counter === targetNum) {
-//             alert("YOU WIN!");
-//             wins++;
-//             resetGame();
-//           }
-      
-//           else if (counter >= targetNum) {
-//             alert("YOU LOST!");
-//             losses++;
-//             resetGame();
-//           }
-
-//       updateDOM();       
-//       return; 
-//       }
 
 function loadQuestion (){
+      //hide and empty previous question elements
       $('#correct-dom').hide();
       $("#correct-dom").empty();
       $('#wrong-dom').hide();
       $("#wrong-dom").empty();
       $('#timeup-dom').hide();
       $("#timeup-dom").empty();
-
+      //populate current question elements
       $('.trivia-dom').show();
       $('#question-dom').html("<h2>" + currentQuestion.question + "</h2>");
 
@@ -333,7 +392,8 @@ function loadQuestion (){
       //   correctAnswer();
       //   wrongAnswer();
       //   unAnswered();
-        nextQuestion();
+      isGameOver();        
+      // nextQuestion();
       //   loadQuestion();
 
         //Next question
@@ -362,14 +422,21 @@ pageLoad();
 
 // Click events
 $("#start").on("click", function() {
-      console.log("game started " + isGameStarted);
+      
       startGame();
-      console.log("game started " + isGameStarted);
+      
       return;
      });
 
+$("#restart").on("click", function() {
+      
+      pageLoad();
+      
+      return;
+     });     
+
 $(".answers-dom").on("click", function() {
-      var selectedAnswer = ($(this).attr("data-answer"));
+      selectedAnswer = ($(this).attr("data-answer"));
       // coinValue = parseInt(coinValue);
       // counter += coinValue;
       // console.log("coin value is " + coinValue );
