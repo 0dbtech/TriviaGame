c828e8e5bd1191b849701315d8d98e68b69b5ec5
admin <admin@admins-MacBook-Pro.local>
2019-02-17 15:29:34 -0500

    content added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,14 @@
 $(document).ready(function() {
 
-console.log("javascript test");
-
-
-
-
+      console.log("javascript test");
 
 //Question Objects START 
 var Q1 = {
-      question:"This is Q1",
-      answer1:"Q1 Answer 1",
-      answer2:"Q1 Answer 2",
-      answer3:"Q1 Answer 3",
-      answer4:"Q1 Answer 4",
+      question:"What is the population size of NYC as of 2016?",
+      answer1:"over 8,000,000",
+      answer2:"over 12,000,000",
+      answer3:"over 5,000,000",
+      answer4:"over 6,000,000",
       correctAnswer: function(){
             return this.answer1;
       },
@@ -22,71 +18,105 @@ var Q1 = {
 };
 
 //access correct answer method
-console.log("testing Q object " + Q1.correctAnswer());
+// console.log("testing Q object " + Q1.correctAnswer());
 
 var Q2 = {
-      question:"This is Q2",
-      answer1:"Q2 Answer 1",
-      answer2:"Q2 Answer 2",
-      answer3:"Q2 Answer 3",
-      answer4:"Q2 Answer 4",
+      question:"In which year was the City of New York established?",
+      answer1:"1622",
+      answer2:"1651",
+      answer3:"1632",
+      answer4:"1624",
       correctAnswer: function(){
-            return this.answer2;
+            return this.answer4;
       },
       //responsive off
       gif:'',
 
 };
-console.log("testing Q object " + Q2.correctAnswer());
 
 var Q3 = {
-      question:"This is Q3",
-      answer1:"Q3 Answer 1",
-      answer2:"Q3 Answer 2",
-      answer3:"Q3 Answer 3",
-      answer4:"Q3 Answer 4",
-      correctAnswer: function(){
-            return this.answer3;
-      },
-       //responsive off
-       gif:'',
-
-};
-console.log("testing Q object " + Q3.correctAnswer());
-
-var Q4 = {
-      question:"This is Q4",
-      answer1:"Q4 Answer 1",
-      answer2:"Q4 Answer 2",
-      answer3:"Q4 Answer 3",
-      answer4:"Q4 Answer 4",
+      question:"Approximately how many people use NYC public transit annually?",
+      answer1:"Way too many",
+      answer2:"800 million",
+      answer3:"3.5 billion",
+      answer4:"1.75 billion",
       correctAnswer: function(){
             return this.answer4;
       },
        //responsive off
+       gif:'<iframe src="https://giphy.com/embed/3oEdv9pgtUVVYdpaY8" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/wnyc-nyc-subway-rat-3oEdv9pgtUVVYdpaY8">via GIPHY</a></p>',
+
+};
+
+var Q4 = {
+      question:"Which is the highest elevated Subway platform in NYC?",
+      answer1:"Woodside-61 St",
+      answer2:"Smith-9th St",
+      answer3:"Van Cortlandt Park-242 St",
+      answer4:"Gun Hill Rd",
+      correctAnswer: function(){
+            return this.answer2;
+      },
+       //responsive off
+       gif:'<iframe src="https://giphy.com/embed/26gsiCIKW7ANEmxKE" width="480" height="244" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/car-oprah-winfrey-26gsiCIKW7ANEmxKE">via GIPHY</a></p>',
+};
+
+var Q5 = {
+      question:"Which of following was NOT filmed at Silvercup Studios in Long Island City?",
+      answer1:"The Sopranos",
+      answer2:"Ugly Betty",
+      answer3:"30 Rock",
+      answer4:"Law & Order",
+      correctAnswer: function(){
+            return this.answer4;
+      },
+       //responsive off
+       gif:'<iframe src="https://giphy.com/embed/141xGebUNJWiGI" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/thread-gold-jamaica-141xGebUNJWiGI">via GIPHY</a></p>',
+};
+
+var Q6 = {
+      question:"Which of the following bridges is used for rail traffic only?",
+      answer1:"Hell Gate Bridge",
+      answer2:"Williamsburg Bridge",
+      answer3:"Queensboro Bridge",
+      answer4:"Manhattan Bridge",
+      correctAnswer: function(){
+            return this.answer1;
+      },
+       //responsive off
        gif:'',
 };
 
-console.log("testing Q object " + Q4.correctAnswer());
+var Q7 = {
+      question:"Which genre of music had its origins in the Bronx, NYC?",
+      answer1:"Disco",
+      answer2:"Hip Hop",
+      answer3:"Jazz",
+      answer4:"Punk Rock",
+      correctAnswer: function(){
+            return this.answer2;
+      },
+       //responsive off
+       gif:'<iframe src="https://giphy.com/embed/d77o7i3tHnKda" width="480" height="324" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/dj-kitty-cat-d77o7i3tHnKda">via GIPHY</a></p>',
+};
 
-//array for question objects
-var questionsArray = [Q1,Q2,Q3,Q4];
-console.log("testing questions array " + questionsArray[0].question);
+
 
 //Question Objects END
 
 //GLOBAL variables
+//array for question objects
+var questionsArray = [Q1,Q2,Q3,Q4,Q5,Q6,Q7];
+      console.log("testing questions array " + questionsArray[0].question);
 var unAnsweredCount = 0;
 var correctCount = 0;
 var wrongCount = 0;
 var selectedAnswer = "";
 //index of current question
 var iQuestionsArray = 0
-
-
 var currentQuestion = questionsArray[iQuestionsArray];
 
-console.log("current question is " + currentQuestion.question);
+      console.log("current question is " + currentQuestion.question);
 
 //functions START
 function pageLoad(){
@@ -99,34 +129,31 @@ function pageLoad(){
       $('.score-dom').hide();
       $('.score-dom').empty();
  
-
       return;
-
       }
 
 function startGame(){
-      console.log("starting game");
+            console.log("starting game");
       
-           //reset gloabl vars for new game DO NOT initialize
-           unAnsweredCount = 0;
-           correctCount = 0;
-           wrongCount = 0;
-           selectedAnswer = "";
-           iQuestionsArray = 0;
-            currentQuestion = questionsArray[iQuestionsArray];
-      console.log("iQuestionsArray:" + iQuestionsArray);     
-      //show answer result container class for new games, populated between questions by append elements
-      $('.qresults-dom').show();
-      $('#start').hide();
-      $('.score-dom').hide();
-      $('.score-dom').empty();
-      // $('.trivia-dom').show();
-      $('#timer-dom').show();
-      $('.trivia-dom').show();
-      loadQuestion();
+      //reset gloabl vars for new game DO NOT initialize
+      unAnsweredCount = 0;
+      correctCount = 0;
+      wrongCount = 0;
+      selectedAnswer = "";
+      iQuestionsArray = 0;
+      currentQuestion = questionsArray[iQuestionsArray];
+            console.log("iQuestionsArray:" + iQuestionsArray);     
+            //show answer result container class for new games, populated between questions by append elements
+            $('.qresults-dom').show();
+            $('#start').hide();
+            $('.score-dom').hide();
+            $('.score-dom').empty();
+            // $('.trivia-dom').show();
+            $('#timer-dom').show();
+            $('.trivia-dom').show();
+            loadQuestion();
 
       return;
-      // isGameStarted = true;
       }
 
 function isGameOver(){
@@ -137,76 +164,71 @@ function isGameOver(){
             
             }
       else{
-     showResults()
+            showResults()
       }
       
       return;
 }
 
 function showResults (){
-      $('.trivia-dom').hide();
-      $('#timer-dom').hide();
-      $('.score-dom').append("<h2>Here's how well you know NYC:</h2>");
-      $('.score-dom').append("<h2>Correct Answers: " + correctCount + "</h2>");
-      $('.score-dom').append("<h2>Wrong Answers: " + wrongCount + "</h2>");
-      $('.score-dom').append("<h2>Unanswered: " + unAnsweredCount + "</h2>");
-      $('.qresults-dom').hide();
-      $('.score-dom').show();
-      $('#start').html("<h3>Play Again</h3>");
-      $('#start').show();
-
-  
-      // var unAnsweredCount = 0;
-      // var correctCount = 0;
-      // var wrongCount = 0;
+            $('.trivia-dom').hide();
+            $('#timer-dom').hide();
+            $('.score-dom').append("<h2>Here's how well you know New York City:</h2>");
+            $('.score-dom').append("<h2>Correct Answers: " + correctCount + "</h2>");
+            $('.score-dom').append("<h2>Wrong Answers: " + wrongCount + "</h2>");
+            $('.score-dom').append("<h2>Unanswered: " + unAnsweredCount + "</h2>");
+            $('.qresults-dom').hide();
+            $('.score-dom').show();
+            $('#start').html("<h3>Play Again</h3>");
+            $('#start').show();
+      return;
 }
 
-//media functions
 
-//END media functions
 
 function wrongAnswer(){
-      console.log("playing WRONG answer");
-      $('.trivia-dom').hide();
-      $("#wrong-dom").append("<h2>Wrong Answer!</h2>");
-      $("#wrong-dom").append("<h2>The correct answer is " + currentQuestion.correctAnswer() + "</h2>");
-      //note 'single' inside "double"
-      //video source relative path based on html location
-      $("#wrong-dom").append("<video id='vid-dom' width='320' height='176' autoplay><source src='assets/videos/wrong.mp4' type='video/mp4'></video>");
+            console.log("playing WRONG answer");
+            $('.trivia-dom').hide();
+            $("#wrong-dom").append("<h2>Wrong Answer!</h2>");
+            $("#wrong-dom").append("<h2>The correct answer is " + currentQuestion.correctAnswer() + "</h2>");
+            //note 'single' inside "double"
+            //video source relative path based on html location
+            $("#wrong-dom").append("<video id='vid-dom' width='320' height='176' autoplay><source src='assets/videos/wrong.mp4' type='video/mp4'></video>");
       var vid = document.getElementById("vid-dom");
       function playVid() {
             vid.play();
             }
-      $('#wrong-dom').show();
-      runTimer2();
-      playVid();
-      wrongCount++;
+            $('#wrong-dom').show();
+            runTimer2();
+            playVid();
+            wrongCount++;
       
-      
-return;
+      return;
 }      
 
 function correctAnswer(){
-      console.log("playing CORRECT answer");
-      $('.trivia-dom').hide();
-      $('#correct-dom').append("<h2>CORRECT!</h2>");
-      $("#correct-dom").append("<h2>" + currentQuestion.correctAnswer() + " is Correct!</h2>");
-      $("#correct-dom").append(currentQuestion.gif);
-      $('#correct-dom').show();
-      runTimer2();
-      correctCount++;
-return;
+            console.log("playing CORRECT answer");
+            $('.trivia-dom').hide();
+            $('#correct-dom').append("<h2>Correct!</h2>");
+            $("#correct-dom").append("<h2>" + currentQuestion.correctAnswer() + " is Correct!</h2>");
+            $("#correct-dom").append(currentQuestion.gif);
+            $('#correct-dom').show();
+            runTimer2();
+            correctCount++;
+
+      return;
 }  
 
 function unAnswered(){
-      console.log("unanswered time up");
-      $('.trivia-dom').hide();
-      $("#timeup-dom").append("<h2>TIME IS UP!</h2>");
-      $("#timeup-dom").append("<h2>The correct answer is " + currentQuestion.correctAnswer() + "</h2>");
-      $('#timeup-dom').show();
-      runTimer2();
-      unAnsweredCount++;
-return;
+            console.log("unanswered time up");
+            $('.trivia-dom').hide();
+            $("#timeup-dom").append("<h2>Time is up!</h2>");
+            $("#timeup-dom").append("<h2>The correct answer is " + currentQuestion.correctAnswer() + "</h2>");
+            $("#timeup-dom").append('<iframe src="https://giphy.com/embed/1XdfVRTyn5d31Q1lG0" width="480" height="428" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/reaction-mood-stephen-colbert-1XdfVRTyn5d31Q1lG0">via GIPHY</a></p>');
+            $('#timeup-dom').show();
+            runTimer2();
+            unAnsweredCount++;
+      return;
 }      
 
 
@@ -228,64 +250,61 @@ function nextQuestion(){
       return;
       }
 
-//Under construction
+
 function checkAnswer(){
-      console.log("checking Answer");
+            console.log("checking Answer");
       
       //PAUSE TIMER 1
       if (selectedAnswer === currentQuestion.correctAnswer()){
-      correctAnswer()
+            correctAnswer()
       
       }
       else {
-      wrongAnswer();
+            wrongAnswer();
       
       }
-      runTimer2();
+      // runTimer2();
       return;
 } 
 
 
-
-
-
 function loadQuestion (){
-      //hide and empty previous question elements
-      $('#correct-dom').hide();
-      $("#correct-dom").empty();
-      $('#wrong-dom').hide();
-      $("#wrong-dom").empty();
-      $('#timeup-dom').hide();
-      $("#timeup-dom").empty();
-      //populate current question elements
-      $('.trivia-dom').show();
-      $('#question-dom').html("<h2>" + currentQuestion.question + "</h2>");
+            //hide and empty previous question elements
+            $('#correct-dom').hide();
+            $("#correct-dom").empty();
+            $('#wrong-dom').hide();
+            $("#wrong-dom").empty();
+            $('#timeup-dom').hide();
+            $("#timeup-dom").empty();
+            //populate current question elements
+            $('.trivia-dom').show();
+            $('#question-dom').html("<h2>" + currentQuestion.question + "</h2>");
 
-      $('#answer1-dom').attr('data-answer', currentQuestion.answer1);
-      $('#answer1-dom').html("<h2>" + currentQuestion.answer1 + "</h2>");
+            $('#answer1-dom').attr('data-answer', currentQuestion.answer1);
+            $('#answer1-dom').html("<h2>" + currentQuestion.answer1 + "</h2>");
 
-      $('#answer2-dom').attr('data-answer', currentQuestion.answer2);
-      $('#answer2-dom').html("<h2>" + currentQuestion.answer2 + "</h2>");
+            $('#answer2-dom').attr('data-answer', currentQuestion.answer2);
+            $('#answer2-dom').html("<h2>" + currentQuestion.answer2 + "</h2>");
 
-      $('#answer3-dom').attr('data-answer', currentQuestion.answer3);
-      $('#answer3-dom').html("<h2>" + currentQuestion.answer3 + "</h2>");
+            $('#answer3-dom').attr('data-answer', currentQuestion.answer3);
+            $('#answer3-dom').html("<h2>" + currentQuestion.answer3 + "</h2>");
 
-      $('#answer4-dom').attr('data-answer', currentQuestion.answer4);
-      $('#answer4-dom').html("<h2>" + currentQuestion.answer4 + "</h2>");
+            $('#answer4-dom').attr('data-answer', currentQuestion.answer4);
+            $('#answer4-dom').html("<h2>" + currentQuestion.answer4 + "</h2>");
 
-      //start clock
-      runTimer();
+            //start clock
+            runTimer();
 
-//test scores
+            //test scores
 
-    console.log("correctCount: " + correctCount);
-    console.log("wrongCount: " + wrongCount);
-    console.log("unAnsweredCount: " + unAnsweredCount);
-    console.log("current question is " + currentQuestion.question);
+            console.log("correctCount: " + correctCount);
+            console.log("wrongCount: " + wrongCount);
+            console.log("unAnsweredCount: " + unAnsweredCount);
+            console.log("current question is " + currentQuestion.question);
     
     return; 
-    //END loadQuestion    
-      }
+//END loadQuestion    
+}
 
 
 //TIMER START////////////
@@ -293,12 +312,12 @@ function loadQuestion (){
     //  Interval Demonstration
     //  Set our number counter to 100.
 //enter time     
-    var timerNumber = 11;
+var timerNumber = 30;
 
 
     //  Variable that will hold our interval ID when we execute
     //  the "run" function
-    var intervalId;
+var intervalId;
 
     //  When the stop button gets clicked, run the stop function.
             //     $("#stop").on("click", stop);
@@ -315,14 +334,14 @@ function loadQuestion (){
       intervalId = setInterval(decrement, 1000);
 //prevent negative counter
 //enter time again     
-     timerNumber = 11;
+     timerNumber = 30;
 
 
     }
 
     //Between questions timer
     function runTimer2() {
-      timerNumber = 3;
+      timerNumber = 6;
       clearInterval(intervalId);
       intervalId = setInterval(decrement2, 1000);
 //prevent negative counter
